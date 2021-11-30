d3744b82d3b4b4419d95065be0fe878367372cfc
rjovelin <richard.jovelin@oicr.on.ca>
2021-11-30 13:40:12 -0500

    added md5sum checks of the pdf

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 cd $1
 
-find . -regex '.*\.txt$' -exec sh -c "wc -l {}" \;
 find . -regex '.*\.txt$' -printf "%f " -exec sh -c "cat {} | tail -n +5 | md5sum" \;
+find . -regex '.*\.pdf$' -printf "%f " -exec sh -c "cat {} | tail -n +7 | md5sum" \;
